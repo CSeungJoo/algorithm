d1e8be31f727b38c25f88082057effe33d8343d8
seungjoo8797100 <seungjoo13579@gmail.com>
2023-05-04 09:33:07 +0900

    10950 cpp clear

---
+++ code/10950.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int T;
+
+    cin >> T;
+
+    for (size_t i = 0; i < T; i++) {
+        int a, b;
+
+        cin >> a >> b;
+
+        cout << a + b << endl;
+    }
+    
+}
