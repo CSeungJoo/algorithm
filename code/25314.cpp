f4e88efc4ea4799428985a39afd8f9355f20ee7d
seungjoo8797100 <seungjoo13579@gmail.com>
2023-05-08 17:36:07 +0900

    25314 cpp clear

---
+++ code/25314.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int N;
+int main(){
+
+    cin >> N;
+    
+    for(int i = 0; i < N/4; i++){
+        cout << "long ";
+        if(i == N/4 - 1) {
+            cout << "int" endl;
+        }
+    }
+}
