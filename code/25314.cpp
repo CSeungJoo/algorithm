6db0e5b5a071e122eb140409bc626988fa2f5150
seungjoo8797100 <seungjoo13579@gmail.com>
2023-05-08 17:37:36 +0900

    25314 cpp bug fix

--- code/25314.cpp
+++ code/25314.cpp
@@ -4,11 +4,11 @@ int N;
 int main(){
 
     cin >> N;
-    
+
     for(int i = 0; i < N/4; i++){
         cout << "long ";
         if(i == N/4 - 1) {
-            cout << "int" endl;
+            cout << "int" << endl;
         }
     }
 }
