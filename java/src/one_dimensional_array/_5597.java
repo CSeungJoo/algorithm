dfa0c9e533f04914cb5ebfc58a647b007f0a2fdc
seungjoo8797100 <seungjoo13579@gmail.com>
2023-06-02 18:08:43 +0900

    5597 java clear

---
+++ java/src/one_dimensional_array/_5597.java
@@ -0,0 +1,16 @@
+package one_dimensional_array;
+import java.util.Scanner;
+public class _5597 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[31];
+        for (int i = 0; i < 28; i++) {
+            int j = sc.nextInt();
+            arr[j] = 1;
+        }
+        for (int i = 1; i < 31; i++) {
+            if (arr[i] != 1) System.out.println(i);
+        }
+    }
+}
