aaf2e517c272f4573313ef67bc4e524ab86d0cf9
seungjoo8797100 <seungjoo13579@gmail.com>
2023-06-03 23:41:59 +0900

    5597 java fix

--- java/src/one_dimensional_array/_5597.java
+++ java/src/one_dimensional_array/_5597.java
@@ -4,7 +4,7 @@ public class _5597 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[31];
+        int[] arr = new int[32];
         for (int i = 0; i < 28; i++) {
             int j = sc.nextInt();
             arr[j] = 1;
