8a96427182ce065454a903b8aeb89dace91ad7c2
seungjoo8797100 <seungjoo13579@gmail.com>
2023-06-05 18:53:12 +0900

    27866 java clear

---
+++ java/src/String/_27866.java
@@ -0,0 +1,12 @@
+package String;
+import java.util.Scanner;
+public class _27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int i = sc.nextInt() - 1;
+        sc.close();
+
+        System.out.println(s.charAt(i));
+    }
+}
