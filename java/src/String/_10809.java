22f5ca61618b0842b8484c4822564caf34cbd6e2
seungjoo8797100 <seungjoo13579@gmail.com>
2023-06-07 20:02:39 +0900

    10809 java clear

---
+++ java/src/String/_10809.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Scanner;
+public class _10809 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] arr = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = s.charAt(i);
+        }
+        for (int i = 0; i < 26; i++) {
+            int result = -1;
+            for (int j = 0; j < s.length(); j++) {
+                if(arr[j] == (char)(97 + i)) {
+                    result = j;
+                    break;
+                }
+            }
+            System.out.print(result +" ");
+        }
+    }
+}
