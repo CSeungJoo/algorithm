d2fd496dd22739f1598630794e9a60f4b37e463a
CSeungJoo <seungjoo13579@gmail.com>
2025-12-27 21:35:13 +0900

    baekjoon java 27323 clear

---
+++ java/src/baekjoon/geometry_1_rectangles_and_triangles/_27323.java
@@ -0,0 +1,14 @@
+package baekjoon.geometry_1_rectangles_and_triangles;
+
+import java.util.Scanner;
+
+public class _27323 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a * b);
+    }
+}
